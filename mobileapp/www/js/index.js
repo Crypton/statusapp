4715361084dcddbc821a9745c9360f83bdb96500
David Dahl <ddahl@nulltxt.se>
2015-07-09 19:02:25 -0500

    added offline.js support

--- mobileapp/www/js/index.js
+++ mobileapp/www/js/index.js
@@ -93,6 +93,42 @@ var app = {
       // check if keychain is supported
       defaultLoginBehavior();
     }
+    // Offline
+    window.Offline.options = {
+      // Should we check the connection status immediatly on page load.
+      checkOnLoad: false,
+
+      // Should we monitor AJAX requests to help decide if we have a connection.
+      interceptRequests: true,
+
+      // Should we automatically retest periodically when the connection is
+      // down (set to false to disable).
+      reconnect: {
+        // How many seconds should we wait before rechecking.
+        initialDelay: 3
+      },
+
+      // Should we store and attempt to remake requests which fail while the
+      // connection is down.
+      requests: true,
+
+      // Should we show a snake game while the connection is down to keep
+      // the user entertained?
+      // It's not included in the normal build, you should bring in
+      // js/snake.js in addition to offline.min.js.
+      game: false,
+
+      // What the xhr checks
+      checks: {
+        xhr: {
+          url: ("https://" +
+                window.crypton.host +
+                (window.crypton.port ? (":" + window.crypton.port) : "") +
+                "/")
+        }
+      }
+    };
+    
   },
 
   APPNAME: 'Kloak',
@@ -740,7 +776,7 @@ var app = {
 
   login: function _login () {
 
-    $('#my-feed-entries').children().reomve();
+    $('#my-feed-entries').children().remove();
     
     var user = $('#username-login').val();
     var pass = $('#password-login').val();
@@ -889,7 +925,7 @@ var app = {
     return this.card.createFingerprintArr(fingerprint).join(" ");
   },
 
-  logNewContct: function _logNewContact(username) {
+  logNewContact: function _logNewContact(username) {
     var html = '<li class="logged-new-contact">Conatct "'
 	  + username + '" added to contacts'
 	  + '</li>';
