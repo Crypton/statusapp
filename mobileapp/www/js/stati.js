c54503f04d554a947a2d779157a0d24e97edab13
David Dahl <ddahl@nulltxt.se>
2015-08-19 16:54:53 -0500

    iphone5 CSS tweaks, license updated to MPL 2.0

--- mobileapp/www/js/stati.js
+++ mobileapp/www/js/stati.js
@@ -1,4 +1,9 @@
-// This file is the application specific code and includes all implemented functions expected by index.js
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// This file is the application specific code and includes all
+// implemented functions expected by index.js
 
 app.localAvatarsPath = 'img/avatars/';
 
